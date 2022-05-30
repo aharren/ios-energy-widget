4cd4f380d4dcd4a365dce0a8f6c61b4fb0e4af72
Arne Harren <ah@0xc0.de>
2022-05-30 13:53:46 +0200

    Rename script to render a data file

--- dev/render-data-file.js
+++ dev/render-data-file.js
@@ -1,22 +1,21 @@
-// render the widget as a png image
+// render a data file for the widget
 
-// usage: node render-raw.js <filename-of-widget> <widget-family> <widget-parameter-string>
+// usage: node render-data-file.js <filename-of-widget> [<widget-parameter-string>]
 
 'use strict';
 
 const fs = require("fs");
-const cv = require('canvas');
 const simulate = require('./lib/simulate.js');
 
 async function main() {
-  if (process.argv.length < 5) {
-    console.log('usage: node render-raw.js <filename-of-widget> <widget-family> <widget-parameter-string>');
+  if (process.argv.length < 3) {
+    console.log('usage: node render-data-file.js <filename-of-widget> [<widget-parameter-string>]');
     process.exit(-1);
   }
 
   const widget = process.argv[2];
-  const widgetFamily = process.argv[3]; // '_raw.response';
-  const widgetParameter = process.argv[4];
+  const widgetFamily = '_raw.response';
+  const widgetParameter = process.argv[3] || 'server=grafana';
 
   const script = fs.readFileSync(widget);
   const settings = {
